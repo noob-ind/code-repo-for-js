dfc8983e606c04b00216029392a6b4c5ba3731bb
Ankit <77172735+noob-ind@users.noreply.github.com>
2024-08-04 21:30:00 +0000

    d1 commit

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,16 @@
+const accountId = 9987
+
+let email = "ankit@gmail.in"
+
+var accountPassword = "12345"
+
+accountCity = "Odisha"
+
+// accountId = 123 cant change its value as it is a constant
+
+email = "shiv@mail.com"
+
+accountPassword = "xyz"
+
+accountCity = "bbsr"
+console.table([accountId,email,accountPassword,accountCity])

3fd59cae06754758d6542108e647b11a3856cf0d
Ankit <77172735+noob-ind@users.noreply.github.com>
2024-08-06 21:13:52 +0000

    d4

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,47 @@
+// let score = "33"
+
+// console.log(typeof score)
+
+// let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber) 
+
+// let score = "33abc"
+
+// console.log(typeof score)
+
+// let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber) 
+
+// console.log(valueInNumber) // will give output as NAN means not a number
+
+// let score = 0
+
+// console.log(typeof score)
+
+// let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber) 
+
+// console.log(valueInNumber) // will give output as null converted to 0
+
+// let isLoggedIn = 1
+
+// let conisLoggedin = Boolean(isLoggedIn)
+
+// console.log(conisLoggedin)
+
+// let isLoggedIn = "" 
+
+// let conisLoggedin = Boolean(isLoggedIn)
+
+// console.log(conisLoggedin) // will give output as false
+
+let strNumber = "33"
+
+let intNumber = Number(strNumber)
+
+console.log(intNumber)
+
+

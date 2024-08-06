9433815b368712bee9b54a019eab4fe4ef049fa4
Ankit <77172735+noob-ind@users.noreply.github.com>
2024-08-06 20:54:10 +0000

    d3

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,20 @@
+"use strict"; //treat all js code as newer version
+
+// alert( 3 + 3) it will not work we are using node js , not browser
+
+console.log(3 + 3)
+
+// number 
+// BigInt
+// String
+// Boolean
+// null = a stand alone value 
+// undefined = now value is not assigned
+// Symbol = unique
+
+// Object 
+
+console.log(typeof null) // output object imp interview qstn
+console.log(typeof undefined)
+
+

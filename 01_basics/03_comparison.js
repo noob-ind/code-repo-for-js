442522ab5c3cda443880d73eca9f9629f10a451a
Ankit <77172735+noob-ind@users.noreply.github.com>
2024-08-11 19:12:34 +0000

    m1

---
+++ 01_basics/03_comparison.js
@@ -0,0 +1,21 @@
+//  console.log("2">1) //"2" implicitly converted to int 2
+  
+//  console.log(null > 0)
+//  console.log(null == 0)
+//  console.log(null >= 0)
+
+//  ******** equality check == and comparison > < <> <= works diffrently
+//  thats why null>=0 is true and null > 0 is false
+
+// console.log(undefined == 0)
+// console.log(undefined > 0)
+// console.log(undefined >= 0)
+
+
+// ****** strict check *****
+// for strict check we use === as here not only value is checked but 
+// also data types
+
+console.log( "2" === 2)
+
+console.log( "2" == 2)
---
+++ 01_basics/dataTypes_summary.js
@@ -0,0 +1,28 @@
+// *** Primitive data types 
+
+// 7 types : String , Number , Boolean , null , undefined, symbol, BigInt
+
+// // reference dat type /Non Primitive data type
+// ex: Array, Objects, Functions
+
+// is javascript static or dynamically typed
+
+// const id = Symbol('123')
+// const anotherId = Symbol('123')
+// console.log(id)
+// console.log(anotherId)
+
+// console.log(id == anotherId)
+
+// Array , Objects , Functions
+
+const heros = ["shaktiman" , "nagraj" , "doga"] //array
+
+// ******* objects *****
+let myObj =
+{
+    name: "Ankit",
+    age: 22
+}
+
+console.log(typeof(myObj))

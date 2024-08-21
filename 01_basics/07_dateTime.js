b80b0a71bc18cc06adb4a257145b645913bec9df
Ankit <77172735+noob-ind@users.noreply.github.com>
2024-08-21 09:15:21 +0000

    k

---
+++ 01_basics/07_dateTime.js
@@ -0,0 +1,20 @@
+let myDate = new Date() 
+// console.log(myDate);
+// console.log(myDate.toString())
+// console.log(myDate.toDateString());
+// console.log(typeof myDate);
+
+
+// let myCreatedDate = new Date(2024, 1, 23)
+
+let myCreatedDate = new Date('2024-01-01')
+
+// console.log(myCreatedDate.toDateString());
+
+
+let myTimeStamp = Date.now()
+
+console.log(myTimeStamp);
+
+
+

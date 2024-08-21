8ea782cadf59c02da4a4d46d6b0afc4f93468ce7
Ankit <77172735+noob-ind@users.noreply.github.com>
2024-08-21 08:40:54 +0000

    numbers

---
+++ 01_basics/06_numbers.js
@@ -0,0 +1,55 @@
+// const score = 400
+// console.log(score);
+
+
+// const balance = new Number(100)
+// console.log(balance)
+
+// console.log(balance.toString().length)
+
+// console.log(balance.toFixed(2));
+
+// const num = 1234.567
+
+// console.log(num.toFixed(2))
+
+// // IMPORMANT INTERVIEW question
+// if precision length is less than the int value then ans will come as exponential
+const num1 = 123.8966
+
+// console.log(num1.toPrecision(2));
+
+const hundrends = 1000000
+// console.log(hundrends.toLocaleString());//us standarnd
+
+// console.log((hundrends.toLocaleString('en-IN')));
+
+//+++++++++++++++++ Maths ++++++++++++++++++++++++++++
+
+
+// console.log(Math)
+// console.log(Math.abs(-4))
+// console.log(Math.round(4.3));
+// console.log(Math.round(4.5));
+// console.log(Math.ceil(4.3));
+// console.log(Math.floor(4.3));
+
+// console.log((Math.min(29,13,40,50)));
+
+
+// console.log(Math.random());
+// console.log(Math.random()*10 + 1);
+
+
+// how to generate random nums b/w 10 and 20
+
+const min = 1000
+const max = 9999
+
+console.log(Math.floor(Math.random() * (max - min) + 1) + min)
+
+
+
+
+
+
